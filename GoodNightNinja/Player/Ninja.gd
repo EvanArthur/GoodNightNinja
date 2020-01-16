ed0f5b0850958a3ee96ddf7ac97963796e6ce466
JustinL1229 <49570253+JustinL1229@users.noreply.github.com>
2020-01-15 19:59:22 -0500

    Made Some Changes

--- GoodNightNinja/Player/Ninja.gd
+++ GoodNightNinja/Player/Ninja.gd
@@ -25,11 +25,11 @@ class_name Ninja
       for moving platforms. """
 
 # Member variables
-const WALK_ACCEL = 800.0
+const WALK_ACCEL = 2000.0
 const WALK_DEACCEL = 1000.0
-const WALK_MAX_VELOCITY = 200.0
+const WALK_MAX_VELOCITY = 500.0
 const AIR_ACCEL = 200.0
-const AIR_DEACCEL = 1000.0
+const AIR_DEACCEL = 200.0
 const JUMP_VELOCITY = 460
 const STOP_JUMP_FORCE = 900.0
 const MAX_SHOOT_POSE_TIME = 0.3
@@ -120,7 +120,7 @@ func _integrate_forces(s):
 	if found_floor:
 		airborne_time = 0.0
 	else:
-		airborne_time += step # Time it spent in the air
+		airborne_time = 200 # Time it spent in the air
 	
 	var on_floor = airborne_time < MAX_FLOOR_AIRBORNE_TIME
 
@@ -131,7 +131,6 @@ func _integrate_forces(s):
 			jumping = false
 		elif not jump:
 			stopping_jump = true
-		
 		if stopping_jump:
 			lv.y += STOP_JUMP_FORCE * step
 	
@@ -142,15 +141,10 @@ func _integrate_forces(s):
 				lv.x -= WALK_ACCEL * step
 		elif move_right and not move_left:
 			if lv.x < WALK_MAX_VELOCITY:
-				lv.x += WALK_ACCEL * step
+				lv.x += WALK_ACCEL * 2*step
 		else:
 			$Sprite.stop()
-			var xv = abs(lv.x)
-			xv -= WALK_DEACCEL * step
-
-			if xv < 0:
-				xv = 0
-			lv.x = sign(lv.x) * xv
+			lv.x = 0
 		
 		# Check jump
 		if not jumping and jump:
@@ -185,12 +179,7 @@ func _integrate_forces(s):
 			if lv.x < WALK_MAX_VELOCITY:
 				lv.x += AIR_ACCEL * step
 		else:
-			var xv = abs(lv.x)
-			xv = 0
-			
-			if xv < 0:
-				xv = 0
-			lv.x = sign(lv.x) * xv
+			lv.x = 0
 		
 		if lv.y < 0:
 			if shoot_time < MAX_SHOOT_POSE_TIME:
