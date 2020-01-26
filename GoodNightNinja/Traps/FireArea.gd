dc94cf32edb4892571e086e6437e560533bca3ef
WillSolow <59705089+WillSolow@users.noreply.github.com>
2020-01-26 15:50:17 -0500

    added firetrap

--- GoodNightNinja/Traps/FireArea.gd
+++ GoodNightNinja/Traps/FireArea.gd
@@ -1,22 +1,17 @@
-extends StaticBody2D
+extends Area2D
 
 onready var timer = $Timer
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var bodies = $BottomPlatform/Spikes.get_overlapping_bodies()
+	var bodies = get_overlapping_bodies()
 	var body = bodies.front()
 	if not bodies.empty():
 		if body.get_name() == "Ninja":
 			if timer.is_stopped():
 				timer.start()
-				body.damage(30)
+				body.damage(3)
 
-
-func _on_Spikes_body_entered(body):
-	if body.get_name() == "Ninja":
-		body.damage(30)
