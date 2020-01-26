25c3a332e272f6c0ee612f463d969ad649510d9f
JustinL1229 <49570253+JustinL1229@users.noreply.github.com>
2020-01-26 16:16:29 -0500

    Revert "added animations"
    This reverts commit bab5a8e8f44004f72b4940c14e4823a82df90ae3, reversing
    changes made to a2940ba1e8f8b6f30687c9c7dad3679f0dc9e6b0.

--- GoodNightNinja/Player/HealthBar/HealthBar.gd
+++ GoodNightNinja/Player/HealthBar/HealthBar.gd
@@ -1,18 +1,19 @@
-extends StaticBody2D
+extends Control
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+onready var health_bar = $HealthBar
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func _on_health_updated(health, amount):
+	health_bar.value = health
+	
+func on_max_health_updated(max_health):
+	health_bar.max_value = max_health
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-
-func _on_Spikes_body_entered(body):
-	if body.get_name() == "Ninja":
-		body.damage(30)
